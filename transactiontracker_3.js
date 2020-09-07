bdb97cc99ce9c877c442a8e8987d50106aa6b3f4
Jill04 <69854931+Jill04@users.noreply.github.com>
2020-09-07 14:09:00 +0530

    Add files via upload

---
+++ transactiontracker_3.js
@@ -0,0 +1,38 @@
+const Web3 = require('web3');
+
+web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/138d2f36cef2452eaaf06661bfd22344' ));
+ function getTransactionsByAccount( startBlockNumber, endBlockNumber) {
+   
+    console.log("Searching for failed transactions  within blocks "  + startBlockNumber + " and " + endBlockNumber);
+
+    for (var i = startBlockNumber; i <= endBlockNumber; i++) {
+        
+        web3.eth.getBlock(i)
+        .then(async function(data)
+        {
+           //console.log("Number",data.number,"Transactions",data.transactions);
+          
+               for( var j=0;j<=data.transactions.length;j++)
+               {
+                let tx = await web3.eth.getTransactionReceipt(data.transactions[j]);
+                
+                if (tx != null) {
+                   
+                           if(!tx.status)
+                           {
+                               console.log({address: tx.from,block:tx.blockNumber,timestamp: new Date()});
+                            }
+    
+                   }
+                
+               }
+            
+       
+        })
+        .catch((err) => console.error(err));
+        
+    }
+} 
+
+
+getTransactionsByAccount(8603562,8603565);
